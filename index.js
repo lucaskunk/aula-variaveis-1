bc3dfab4453fe6b1ca735a3847f1bd68f24a18f1
Alejandro <aibarria92@gmail.com>
2023-08-16 12:40:02 -0300

    ejercicio 1

---
+++ index.js
@@ -0,0 +1,47 @@
+/*Exercícios de interpretação de código
+1- console.log(10, 5)
+2- console.log(20, 10, 10)
+3- primeira variavel: horasTrabalhadasDia
+   segunda variavel: salarioDiario
+*/
+
+//Exercícios de escrita de código
+//1
+//a
+//let nome
+
+//b
+//let idade
+
+//c
+//typeof nome
+//typeof idade
+
+//console.log(typeof nome);
+//console.log(typeof idade);
+
+//d
+/*como nao foi atribuido nenhum valor 
+para variavel nos for retornado: undefined */
+
+//e
+//let nome = prompt("Cual o seu nome?");
+//let idade = prompt("Cuala sua idade?");
+
+//console.log(nome, idade);
+
+//f
+//typeof nome;
+//typeof idade;
+
+//console.log(typeof nome);
+//onsole.log(typeof idade);
+
+//como foi atribuido um valor  a variavel, virou um "string"
+
+//g
+let nome = prompt("Cual o seu nome?");
+let idade = prompt("Cuala sua idade?");
+
+console.log("Olá", nome, "você tem", idade, "anos");
+
